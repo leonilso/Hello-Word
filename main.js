8cd26161bda42ef39a92614c298c7753ae0afdcb
Leonilso <129322347+leonilso@users.noreply.github.com>
2024-05-17 00:25:38 -0300

    corrigindo estilo e formatação

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 let container = document.getElementById("container")
 
-container.addEventListener( "mouseleave", event => {
+container.addEventListener( "mouseover", event => {
     container.style.width = "800"
     container.style.height = "600"
 })
-container.addEventListener( "mouseout", event => {
+container.addEventListener( "mouseleave", event => {
     container.style.width = "600"
     container.style.height = "400"
 })
